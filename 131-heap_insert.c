f54e6fb933a4dbb5ea84a83e0ea26c7942846600
Ben Vilakazi <116912017+BenVilakazi@users.noreply.github.com>
2024-02-01 17:50:30 +0200

    Update 131-heap_insert.c

--- 131-heap_insert.c
+++ 131-heap_insert.c
@@ -1,5 +1,4 @@
-#include "binary_trees.h"
-
+#include "binary_trees.h" 
 /**
  * heap_insert - inserts a value in Max Binary Heap
  * @root: a double pointer to the root node of the Heap to insert the value
